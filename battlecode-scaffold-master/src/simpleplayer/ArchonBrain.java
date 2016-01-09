3cf3c9b3e2f351cfd60edc58e7a7ed1502dc2a70
Alex Wu <alex@Lolz.local>
2016-01-09 08:04:52 -0800

    Archon brain organizational structure
    initialize and runTurn methods

--- battlecode-scaffold-master/src/simpleplayer/ArchonBrain.java
+++ battlecode-scaffold-master/src/simpleplayer/ArchonBrain.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import org.omg.CORBA.INITIALIZE;
+
 import battlecode.common.Clock;
 import battlecode.common.Direction;
 import battlecode.common.GameConstants;
@@ -34,34 +36,7 @@ public class ArchonBrain implements Brain {
 
 	private MapLocation start;
 
-	@Override
-	public void run(RobotController rc) {
-		if (start == null)
-			start = rc.getLocation();
-
-		if (firstRun) {
-			try {
-				firstRun = false;
-				// rc.broadcastSignal(20000);//2*sightRange ); //maximize
-				// broadcast
-				// range without costing extra stuff
-				rc.broadcastSignal(100 * 100);// May as well broadcast the
-												// entire
-												// map.
-				Clock.yield();
-
-				Signal[] signals = rc.emptySignalQueue();
-				for (Signal s : signals)
-					archonStarts.put(s.getID(), s.getLocation());
-
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-				e.printStackTrace();
-			}
-		}
-
- 
-		 while (true) {
+	private void intialize(RobotController rc) {
 		try {
 
 			Signal[] signals = rc.emptySignalQueue();
@@ -92,7 +67,48 @@ public class ArchonBrain implements Brain {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
-		 }
+	}
+
+	private void runTurn(RobotController rc) {
+		try {
+
+			Signal[] signals = rc.emptySignalQueue();
+			int numGuards = 0;
+
+			MapLocation com = com(archonStarts.values(), start);
+			// TODO: make building occur in all directions.
+			if (com.distanceSquaredTo(rc.getLocation()) <= 4 || !rc.canMove(rc.getLocation().directionTo(com))) {
+				for (Direction d : Direction.values()) {
+					if (numGuards < 4 && rc.canBuild(d, RobotType.GUARD)) {
+						rc.build(d, RobotType.GUARD);
+						numGuards++;
+						rc.setIndicatorString(2, numGuards + " guards total");
+					}
+				}
+				for (Direction d : Direction.values()) {
+
+					if (rc.canBuild(d, RobotType.SOLDIER)) {
+						rc.build(d, RobotType.SOLDIER);
+					}
+				}
+			} else {
+				rc.move(rc.getLocation().directionTo(com));
+
+			}
+			Clock.yield();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run(RobotController rc) {
+		intialize(rc);
+
+		while (true) {
+			runTurn(rc);
+		}
 	}
 
 }
