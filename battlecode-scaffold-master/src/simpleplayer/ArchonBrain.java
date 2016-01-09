d2b144575ac52e768275cd999f1cf1a4a472cc2e
Alex Wu <alex@Lolz.local>
2016-01-09 07:56:34 -0800

    committing
    why not?

--- battlecode-scaffold-master/src/simpleplayer/ArchonBrain.java
+++ battlecode-scaffold-master/src/simpleplayer/ArchonBrain.java
@@ -1,23 +1,26 @@
 package simpleplayer;
 
-import java.util.*;
-import battlecode.common.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import battlecode.common.Clock;
+import battlecode.common.Direction;
+import battlecode.common.GameConstants;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+import battlecode.common.Signal;
 
 public class ArchonBrain implements Brain {
 	// still doesn't account for own location
 	private boolean firstRun = true;
 	private Map<Integer, MapLocation> archonStarts = new HashMap<>(6);
-	private RobotController rc;
-	private static Direction[] directions = { Direction.NORTH, Direction.NORTH_EAST, Direction.EAST,
-			Direction.SOUTH_EAST, Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST };
-	private static RobotType[] robotTypes = { RobotType.SCOUT, RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
-			RobotType.GUARD, RobotType.GUARD, RobotType.VIPER, RobotType.TURRET };
-	private static int myAttackRange = 24;
-	private static int sightRange = 35;
 
-	private MapLocation com(Collection<MapLocation> locs) {
-		int x = rc.getLocation().x;
-		int y = rc.getLocation().y;
+	private MapLocation com(Collection<MapLocation> locs, MapLocation self) {
+		int x = self.x;
+		int y = self.y;
 		for (MapLocation loc : locs) {
 			x += loc.x;
 			y += loc.y;
@@ -29,99 +32,67 @@ public class ArchonBrain implements Brain {
 		}
 	}
 
+	private MapLocation start;
+
 	@Override
-	public void run(RobotController rcI) {
-		rc = rcI;
+	public void run(RobotController rc) {
+		if (start == null)
+			start = rc.getLocation();
 
-		MapLocation start = rc.getLocation();
-
-		try {
-			rc.broadcastSignal(20000);// 2*sightRange ); //maximize broadcast
-										// range without costing extra stuff
-			Clock.yield();
-
-			Signal[] signals = rc.emptySignalQueue();
-			for (Signal s : signals) {
-				archonStarts.put(s.getID(), s.getLocation());
-
-			}
-
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-		}
-
-		while (true) {
+		if (firstRun) {
 			try {
-				// rc.broadcastSignal(sightRange * 2); // free broadcasting (
-				// should add to everything )
-				Signal[] signals = rc.emptySignalQueue();
-				RobotInfo[] nearby =  rc.senseNearbyRobots();
-				int numGuards = 0;
-				for (RobotInfo nej: nearby){
-					if (nej.type.equals(RobotType.GUARD)){
-						numGuards +=1;
-					}
-				}
-				MapLocation com = com(archonStarts.values());
-				if (com.distanceSquaredTo(rc.getLocation()) <= 4 || !rc.canMove(rc.getLocation().directionTo(com))) {
-					for (int i = 0; i < directions.length; i++) {
-						if (numGuards <= 4 && rc.canBuild(directions[i], RobotType.GUARD)) {
-							rc.build(directions[i], RobotType.GUARD);
-							break;
-						}
-					}
-					for (int i = 0; i < directions.length; i++) {
-						if (rc.canBuild(directions[i], RobotType.SOLDIER)) { //
-							rc.build(directions[i], RobotType.SOLDIER);
-							break;
-						}
-
-					}
-
-				} else {
-					/*
-					 * int index=0; for (int i = 0; i < 8;i++){ if
-					 * (directions[i].equals(rc.getLocation().directionTo(com)))
-					 * { index = i; } } if
-					 * (rc.canMove(rc.getLocation().directionTo(com))){
-					 * rc.move(rc.getLocation().directionTo(com)); } else if
-					 * (rc.canMove(directions[(index+1)%8])){
-					 * rc.move(directions[(index+1)%8]); } else{
-					 * rc.move(directions[(index-1)%8]);
-					 */
-					rc.move(rc.getLocation().directionTo(com));
-
-				}
+				firstRun = false;
+				// rc.broadcastSignal(20000);//2*sightRange ); //maximize
+				// broadcast
+				// range without costing extra stuff
+				rc.broadcastSignal(100 * 100);// May as well broadcast the
+												// entire
+												// map.
 				Clock.yield();
+
+				Signal[] signals = rc.emptySignalQueue();
+				for (Signal s : signals)
+					archonStarts.put(s.getID(), s.getLocation());
+
 			} catch (Exception e) {
 				System.out.println(e.getMessage());
 				e.printStackTrace();
 			}
 		}
+
+ 
+		 while (true) {
+		try {
+
+			Signal[] signals = rc.emptySignalQueue();
+			int numGuards = 0;
+
+			MapLocation com = com(archonStarts.values(), start);
+			// TODO: make building occur in all directions.
+			if (com.distanceSquaredTo(rc.getLocation()) <= 4 || !rc.canMove(rc.getLocation().directionTo(com))) {
+				for (Direction d : Direction.values()) {
+					if (numGuards < 4 && rc.canBuild(d, RobotType.GUARD)) {
+						rc.build(d, RobotType.GUARD);
+						numGuards++;
+						rc.setIndicatorString(2, numGuards + " guards total");
+					}
+				}
+				for (Direction d : Direction.values()) {
+
+					if (rc.canBuild(d, RobotType.SOLDIER)) {
+						rc.build(d, RobotType.SOLDIER);
+					}
+				}
+			} else {
+				rc.move(rc.getLocation().directionTo(com));
+
+			}
+			Clock.yield();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		 }
 	}
 
 }
-
-/*
- * int fate = rand.nextInt(1000); // Check if this ARCHON's core is ready if
- * (fate % 10 == 2) { // Send a message signal containing the data (6370, 6147)
- * rc.broadcastMessageSignal(6370, 6147, 80); } Signal[] signals =
- * rc.emptySignalQueue(); if (signals.length > 0) { // Set an indicator string
- * that can be viewed in the client rc.setIndicatorString(0,
- * "I received a signal this turn!"); } else { rc.setIndicatorString(0,
- * "I don't any signal buddies"); } if (rc.isCoreReady()) { if (fate < 800) { //
- * Choose a random direction to try to move in Direction dirToMove =
- * directions[fate % 8]; // Check the rubble in that direction if
- * (rc.senseRubble( rc.getLocation().add(dirToMove)) >=
- * GameConstants.RUBBLE_OBSTRUCTION_THRESH) { // Too much rubble, so I should
- * clear it rc.clearRubble(dirToMove); // Check if I can move in this direction
- * } else if (rc.canMove(dirToMove)) { // Move rc.move(dirToMove); } } else { //
- * Choose a random unit to build RobotType typeToBuild = robotTypes[fate % 8];
- * // Check for sufficient parts if (rc.hasBuildRequirements(typeToBuild)) { //
- * Choose a random direction to try to build in Direction dirToBuild =
- * directions[rand.nextInt(8)]; for (int i = 0; i < 8; i++) { // If possible,
- * build in this direction if (rc.canBuild(dirToBuild, typeToBuild)) {
- * rc.build(dirToBuild, typeToBuild); break; } else { // Rotate the direction to
- * try dirToBuild = dirToBuild.rotateLeft(); } } } } }
- */
