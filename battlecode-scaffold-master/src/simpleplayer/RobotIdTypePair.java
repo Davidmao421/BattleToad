eae073db2d87729efccbf29e04c965187d8ceff7
Alex Wu <alex@Lolz.local>
2016-01-09 17:58:38 -0800

    Forgot to save
    now i did

--- battlecode-scaffold-master/src/simpleplayer/RobotIdTypePair.java
+++ battlecode-scaffold-master/src/simpleplayer/RobotIdTypePair.java
@@ -1,5 +1,26 @@
 package simpleplayer;
 
-public class RobotIdTypePair {
+import battlecode.common.RobotType;
+import battlecode.common.Team;
 
+public class RobotIdTypePair {
+	public int id;
+	public RobotType type;
+	public RobotIdTypePair(int id, RobotType type) {
+		super();
+		this.id = id;
+		this.type = type;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public RobotType getType() {
+		return type;
+	}
+	public void setType(RobotType type) {
+		this.type = type;
+	}
 }
