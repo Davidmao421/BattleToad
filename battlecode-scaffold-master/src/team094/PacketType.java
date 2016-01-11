733c3a8db0ab1ca27322d69ac1f2a9015067997f
Alex Wu <alex@Lolz.local>
2016-01-10 19:08:21 -0800

    Signal Encoder Skeleton set
    Need to fill out all the TODO’s

--- battlecode-scaffold-master/src/team094/PacketType.java
+++ battlecode-scaffold-master/src/team094/PacketType.java
@@ -3,6 +3,12 @@ package team094;
 public enum PacketType {
 	   ECHO(0),
 	   NEW_ROBOT(1),
+	   ATTACK_ENEMY(2),
+	   PANIC(3),
+	   PANIC_OVER(4),
+	   DEAD(5),
+	   CHANGE_SCHEME(6),
+	   LOCAL_ATTACK(7),
 	    OTHER(31);
 
 	    public final int header;
