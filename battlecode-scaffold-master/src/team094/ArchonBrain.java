cfb414014d93c467c5ae2ed76639b4c23c3dacd4
Alex Wu <alex@Lolz.local>
2016-01-10 21:01:32 -0800

    Archon Routines
    All archon routines other than charge are done

--- battlecode-scaffold-master/src/team094/ArchonBrain.java
+++ battlecode-scaffold-master/src/team094/ArchonBrain.java
@@ -1,5 +1,6 @@
 package team094;
 
+import java.awt.Robot;
 import java.util.*;
 import battlecode.common.*;
 
@@ -10,10 +11,23 @@ public class ArchonBrain implements Brain {
 	}
 
 	private int turns;
+	private Routine last;
 	private Routine current;
 
-	private boolean buildRobot(RobotController rc, RobotType type) {
+	private static final int BROADCAST_RANGE = 70;
+
+	private void setRoutine(Routine r) {
+		last = current;
+		current = r;
+	}
+
+	private boolean buildRobot(RobotController rc, RobotType type) throws GameActionException {
 		ArrayList<Direction> list = new ArrayList<>(Arrays.asList(Statics.directions));
+		if (!rc.hasBuildRequirements(type)){
+			scavenge(rc);
+			turns--;
+			return false;
+		}
 		for (int i = 0; list.size() > 0; i++) {
 			if (rc.canBuild(list.get(i), type)) {
 				try {
@@ -27,44 +41,122 @@ public class ArchonBrain implements Brain {
 		return false;
 	}
 
-	private boolean buildScout(RobotController rc) {
+	private boolean buildScout(RobotController rc) throws GameActionException {
 		return buildRobot(rc, RobotType.SCOUT);
 	}
 
-	private void intialize(RobotController rc) {
+	private void intialize(RobotController rc) throws GameActionException {
 		turns = 0;
+
 		buildScout(rc);
 		current = Routine.TURRET_CLUSTER;
 	}
 
-	private void buildTurretCluster(RobotController rc) {
+	private void buildTurretCluster(RobotController rc) throws GameActionException {
 		switch (turns) {
 		case 1:
 		case 2:
-		case 4:
+		case 3:
 			buildRobot(rc, RobotType.SOLDIER);
 			break;
-		case 3:
+		case 4:
 			buildRobot(rc, RobotType.TURRET);
+			RobotInfo[] robots = rc.senseNearbyRobots();
+			for (RobotInfo r : robots)
+				if (r.team == rc.getTeam() && r.type == RobotType.TURRET
+						&& r.location.distanceSquaredTo(rc.getLocation()) == 1) {
+					Signal s = SignalEncoder.encodeRobot(r.type, r.ID, r.location);
+					rc.broadcastMessageSignal(s.getMessage()[0], s.getMessage()[1], BROADCAST_RANGE);
+				}
 			break;
 		default:
-			current = Routine.NONE;
+			setRoutine(Routine.NONE);
 			turns = 0;
 			break;
 		}
 	}
 
-	private void runTurn(RobotController rc) {
+	private void scavenge(RobotController rc) throws GameActionException {
+		if (turns > 5) {
+			turns = 0;
+			return;
+		}
+
+		ArrayList<Direction> potential = new ArrayList<>();
+		double maxParts = 0;
+		for (int i = 0; i < Statics.directions.length; i++) {
+			double parts = rc.senseParts(rc.getLocation().add(Statics.directions[i]));
+			if (parts == maxParts)
+				if (rc.canMove(Statics.directions[i]))
+					potential.add(Statics.directions[i]);
+
+				else if (parts > maxParts) {
+					maxParts = parts;
+					if (rc.canMove(Statics.directions[i])) {
+						potential.clear();
+						potential.add(Statics.directions[i]);
+					}
+				}
+		}
+		if (potential.size() > 0)
+			rc.move(potential.get((int) (Math.random() * potential.size())));
+
+	}
+	
+	Direction _moveDirection;
+	private void randomlyMove(RobotController rc) throws GameActionException{
+		if (turns > 5){
+			_moveDirection = null;
+			turns = 0;
+			return;
+		}
+		
+		if (_moveDirection == null)
+			_moveDirection = Statics.directions[(int) (Math.random()*Statics.directions.length)];
+		
+		if (rc.canMove(_moveDirection)){
+			rc.move(_moveDirection);
+			return;
+		}
+		
+		ArrayList<Direction> potential = new ArrayList<>(Arrays.asList(Statics.directions));
+		while (potential.size() > 0){
+			Direction d = potential.remove((int)(Math.random()*potential.size()));
+			if (rc.canMove(d)){
+				_moveDirection = d;
+				rc.move(d);
+				return;
+			}
+		}
+	}
+	
+	private void determineRoutine(RobotController rc){
+		if (last != Routine.TURRET_CLUSTER)
+			setRoutine(Routine.TURRET_CLUSTER);
+		else 
+			setRoutine(Routine.SCAVENGE);
+	}
+	
+	private void charge(RobotController rc){
+		//TODO: Implement charge
+	}
+
+	private void runTurn(RobotController rc) throws GameActionException {
 		switch (current) {
 		case CHARGE:
 			break;
 		case NONE:
-			break;
+			determineRoutine(rc);
+			runTurn(rc);
+			return;
 		case RANDOM:
+			randomlyMove(rc);
 			break;
 		case SCAVENGE:
+			scavenge(rc);
 			break;
 		case SCOUT:
+			buildScout(rc);
 			break;
 		case TURRET_CLUSTER:
 			buildTurretCluster(rc);
@@ -76,12 +168,20 @@ public class ArchonBrain implements Brain {
 
 	@Override
 	public void run(RobotController rc) {
-		intialize(rc);
+		try {
+			intialize(rc);
+		} catch (GameActionException e1) {
+			e1.printStackTrace();
+		}
 
 		while (true) {
 			Clock.yield();
 			turns++;
-			runTurn(rc);
+			try {
+				runTurn(rc);
+			} catch (GameActionException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
