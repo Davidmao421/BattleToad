1dec75a6e138d70b5c8f2685bd72a974af5cd774
Alex Wu <alex@Lolz.local>
2016-01-10 20:11:18 -0800

    Archon basic routine
    Building routine done for archons

--- battlecode-scaffold-master/src/team094/ArchonBrain.java
+++ battlecode-scaffold-master/src/team094/ArchonBrain.java
@@ -4,96 +4,73 @@ import java.util.*;
 import battlecode.common.*;
 
 public class ArchonBrain implements Brain {
-	// still doesn't account for own location
-	private Map<Integer, MapLocation> archonStarts = new HashMap<>(6);
-	private Map<Integer, RobotType> robots = new HashMap<>(100);
 
-	private MapLocation com(Collection<MapLocation> locs) {
-		int x, y = x = 0;
-		for (MapLocation loc : locs) {
-			x += loc.x;
-			y += loc.y;
+	private enum Routine {
+		TURRET_CLUSTER, CHARGE, SCAVENGE, RANDOM, NONE, SCOUT
+	}
+
+	private int turns;
+	private Routine current;
+
+	private boolean buildRobot(RobotController rc, RobotType type) {
+		ArrayList<Direction> list = new ArrayList<>(Arrays.asList(Statics.directions));
+		for (int i = 0; list.size() > 0; i++) {
+			if (rc.canBuild(list.get(i), type)) {
+				try {
+					rc.build(list.get(i), type);
+					return true;
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
 		}
-		return new MapLocation(x / Math.max(locs.size(), 1), y / Math.max(1, locs.size()));
+		return false;
+	}
 
+	private boolean buildScout(RobotController rc) {
+		return buildRobot(rc, RobotType.SCOUT);
 	}
 
 	private void intialize(RobotController rc) {
-		try {
-			rc.broadcastSignal(500);
+		turns = 0;
+		buildScout(rc);
+		current = Routine.TURRET_CLUSTER;
+	}
 
-			Clock.yield();
-
-			Signal[] signals = rc.emptySignalQueue();
-			for (Signal s : signals)
-				archonStarts.put(s.getID(), s.getLocation());
-
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
+	private void buildTurretCluster(RobotController rc) {
+		switch (turns) {
+		case 1:
+		case 2:
+		case 4:
+			buildRobot(rc, RobotType.SOLDIER);
+			break;
+		case 3:
+			buildRobot(rc, RobotType.TURRET);
+			break;
+		default:
+			current = Routine.NONE;
+			turns = 0;
+			break;
 		}
 	}
 
 	private void runTurn(RobotController rc) {
-		try {
-			int numGuards = 0;
-
-			Signal[] signals = rc.emptySignalQueue();
-			for (Signal s : signals) {
-				RobotIdTypePair pair = SignalEncoder.decodeRobot(s);
-				if (robots.containsKey(pair.id))
-					continue;
-				robots.put(pair.id, pair.type);
-			}
-			RobotInfo[] nearbyGuards =  rc.senseNearbyRobots();
-			for (RobotInfo nej: nearbyGuards){
-				if (nej.type.equals(RobotType.GUARD)){
-					numGuards +=1;
-				}
-			}
-
-			MapLocation com = com(archonStarts.values());
-			rc.setIndicatorString(1, "("+com.x+", "+com.y+")");
-			if (!(com.distanceSquaredTo(rc.getLocation()) <= 4 || !rc.canMove(rc.getLocation().directionTo(com)))) {
-				rc.move(rc.getLocation().directionTo(com));
-			}
-			else {
-				ArrayList<Direction> dirs = new ArrayList<Direction>(Arrays.asList(Statics.directions));
-				Direction[] dirs2 = new Direction[dirs.size()];
-				int i=0;
-				while(dirs.size()>0){
-					Direction d=dirs.get((int)(8*Math.random()));
-					if (numGuards > 8)
-						break;
-					if (rc.canBuild(d, RobotType.GUARD)) {
-						rc.build(d, RobotType.GUARD);
-//						numGuards++;
-//						numGuards = 0;
-						rc.setIndicatorString(2, numGuards + " guards total");
-						break;
-					}			
-					dirs2[i++]=d;
-				}
-				for(; dirs.size()>0;i++)
-					dirs2[i] = dirs.remove((int)(dirs.size()*Math.random()));
-				
-				for (Direction d : dirs2) {
-					if (rc.canBuild(d, RobotType.SOLDIER)) {
-						rc.build(d, RobotType.SOLDIER);
-					}
-				}
-
-				RobotInfo[] nearby = rc.senseNearbyRobots();
-				for (RobotInfo info : nearby) {
-					if (robots.containsKey(info.ID) || info.team != rc.getTeam())
-						continue;
-					robots.put(info.ID, info.type);
-					rc.broadcastMessageSignal(SignalEncoder.encodeRobot(info.type, info.ID, com).getMessage()[0], 0, 70);
-				}
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
+		switch (current) {
+		case CHARGE:
+			break;
+		case NONE:
+			break;
+		case RANDOM:
+			break;
+		case SCAVENGE:
+			break;
+		case SCOUT:
+			break;
+		case TURRET_CLUSTER:
+			buildTurretCluster(rc);
+			break;
+		default:
+			break;
 		}
 	}
 
@@ -103,6 +80,7 @@ public class ArchonBrain implements Brain {
 
 		while (true) {
 			Clock.yield();
+			turns++;
 			runTurn(rc);
 		}
 	}
