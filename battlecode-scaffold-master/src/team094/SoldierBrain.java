6f40b7bab3745e72cb72c9a1cfc03ab8d3b22a55
Davidmao421 <davidmao421@gmail.com>
2016-01-15 16:23:22 -0800

    exceptional clean up for soldiers

--- battlecode-scaffold-master/src/team094/SoldierBrain.java
+++ battlecode-scaffold-master/src/team094/SoldierBrain.java
@@ -12,27 +12,28 @@ public class SoldierBrain implements Brain {
 	private static ArrayList<MapLocation> past = new ArrayList<MapLocation>();
 	private static Direction[] directions = { Direction.NORTH, Direction.NORTH_EAST, Direction.EAST,
 			Direction.SOUTH_EAST, Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST };
-	private static ArrayList <MapLocation> targets = new ArrayList<MapLocation> ();
+	private static ArrayList<MapLocation> targets = new ArrayList<MapLocation>();
 	RobotController rc;
+
 	@Override
 	public void run(RobotController inRc) {
-		rc=inRc;
-		if (Math.random() < 0.5){ //TODO: implement passing of integer to determine Soldier type
+		rc = inRc;
+		if (Math.random() < 0.5) { // TODO: implement passing of integer to
+									// determine Soldier type
 			while (true) {
-				try{
-					runTurnGuard(rc);
+				try {
+					runTurn(rc);
 					Clock.yield();
-				}catch(Exception e){
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
-		}
-		else{
+		} else {
 			while (true) {
-				try{
-					runTurn(rc);
+				try {
+					runTurnGuard(rc);
 					Clock.yield();
-				}catch(Exception e){
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
@@ -40,8 +41,9 @@ public class SoldierBrain implements Brain {
 	}
 
 	public void intialize(RobotController rc) {
-		
+
 	}
+
 	public static void runTurnPack(RobotController rc) throws GameActionException {
 		int myAttackRange = rc.getType().attackRadiusSquared;
 		Team myTeam = rc.getTeam();
@@ -64,32 +66,35 @@ public class SoldierBrain implements Brain {
 				rc.attackLocation(zombiesWithinRange[0].location);
 			}
 		}
-		if (!shouldAttack){
+		if (!shouldAttack) {
 			Signal[] signals = rc.emptySignalQueue();
-			for (Signal hej: signals){
-				MapLocation naw = new MapLocation (hej.getMessage()[0],hej.getMessage()[1]); // TODO: want more efficient bit usage
-				if (!targets.contains(naw)){
-					targets.add(naw); // TODO: Need someway to remove locations with scout saying that the target is dead.
+			for (Signal hej : signals) {
+				MapLocation naw = new MapLocation(hej.getMessage()[0], hej.getMessage()[1]); // TODO:
+																								// want
+																								// more
+																								// efficient
+																								// bit
+																								// usage
+				if (!targets.contains(naw)) {
+					targets.add(naw); // TODO: Need someway to remove locations
+										// with scout saying that the target is
+										// dead.
 				}
 			}
 			MapLocation closest;
 			int closestDistance = Integer.MAX_VALUE;
-			if (!targets.isEmpty()){
-				for (MapLocation i: targets){
-					if (rc.getLocation().distanceSquaredTo(i) < closestDistance){
+			if (!targets.isEmpty()) {
+				for (MapLocation i : targets) {
+					if (rc.getLocation().distanceSquaredTo(i) < closestDistance) {
 						closest = i;
 						closestDistance = rc.getLocation().distanceSquaredTo(i);
 					}
 				}
 			} // TODO: Finish Everything else
-			
-			
-			
 
 		}
 	}
-	
-	
+
 	public static void runTurnGuard(RobotController rc) throws GameActionException {
 		int myAttackRange = rc.getType().attackRadiusSquared;
 		Team myTeam = rc.getTeam();
@@ -118,11 +123,11 @@ public class SoldierBrain implements Brain {
 				}
 			}
 		}
-		if (!shouldAttack) {
-			
+		if (!shouldAttack && rc.isCoreReady()) {
+
 			RobotInfo[] nearby = rc.senseNearbyRobots();
 			int numArchons = 0;
-			LinkedList<MapLocation> arcLoc = new LinkedList<MapLocation>();
+			ArrayList<MapLocation> arcLoc = new ArrayList<MapLocation>();
 			for (RobotInfo naw : nearby) {
 				if (naw.type.equals(RobotType.ARCHON)) {
 					arcLoc.add(naw.location);
@@ -131,85 +136,49 @@ public class SoldierBrain implements Brain {
 			}
 			int nearestArc = 0;
 			int shortestDistance = Int.MaxValue();
-			for (int i = 0; i < arcLoc.size(); i++) {
-				int distance = rc.getLocation().distanceSquaredTo(arcLoc.get(i));
-				if (distance <= shortestDistance) {
-					shortestDistance = distance;
-					nearestArc = i;
-				}
-			}
-			if (shortestDistance < 4 ){
-				if (rc.canMove(arcLoc.get(nearestArc).directionTo(rc.getLocation()))) {
-					rc.move(arcLoc.get(nearestArc).directionTo(rc.getLocation()));
-				}
-				else{
-					int rand = (int)(2 * Math.random());
-					boolean left = rc.canMove(arcLoc.get(nearestArc).directionTo(rc.getLocation()).rotateLeft());
-					boolean right = rc.canMove((arcLoc.get(nearestArc).directionTo(rc.getLocation()).rotateRight()));
-					if (left && right){
-						if (rand ==1){
-							rc.move(arcLoc.get(nearestArc).directionTo(rc.getLocation()).rotateLeft());
-						}
-						if (rand == 2){
-							rc.move(arcLoc.get(nearestArc).directionTo(rc.getLocation()).rotateRight());
-						}
-					}
-					else if (left && !right){
-						rc.move(arcLoc.get(nearestArc).directionTo(rc.getLocation()).rotateLeft());
-					}
-					else if (right && !left){
-						rc.move(arcLoc.get(nearestArc).directionTo(rc.getLocation()).rotateRight());
+			if (arcLoc.size() != 0) {
+				for (int i = 0; i < arcLoc.size(); i++) {
+					int distance = rc.getLocation().distanceSquaredTo(arcLoc.get(i));
+					if (distance <= shortestDistance) {
+						shortestDistance = distance;
+						nearestArc = i;
 					}
 				}
-			}
-			else if (shortestDistance >= 9) {
-				if (rc.canMove(rc.getLocation().directionTo(arcLoc.get(nearestArc)))) {
-					rc.move(rc.getLocation().directionTo(arcLoc.get(nearestArc)));
-				}
-				else{
-					int rand = (int)(2 * Math.random());
-					boolean left = rc.canMove(rc.getLocation().directionTo(arcLoc.get(nearestArc)).rotateLeft());
-					boolean right = rc.canMove(rc.getLocation().directionTo(arcLoc.get(nearestArc)).rotateRight());
-					if (left && right){
-						if (rand ==1){
-							rc.move(rc.getLocation().directionTo(arcLoc.get(nearestArc)).rotateLeft());
-						}
-						if (rand == 2){
-							rc.move(rc.getLocation().directionTo(arcLoc.get(nearestArc)).rotateRight());
+				if (shortestDistance < 4) {
+					coolMethods.moveTo(arcLoc.get(nearestArc).directionTo(rc.getLocation()), rc);
+				} else if (shortestDistance >= 9) {
+					coolMethods.moveTo(rc.getLocation().directionTo(arcLoc.get(nearestArc)), rc);
+				} else {
+					boolean left = rc
+							.canMove(arcLoc.get(nearestArc).directionTo(rc.getLocation()).rotateLeft().rotateLeft());
+					boolean right = rc
+							.canMove(arcLoc.get(nearestArc).directionTo(rc.getLocation()).rotateRight().rotateRight());
+					if (left && right) {
+						if (Math.random() < 0.5) {
+							rc.move(arcLoc.get(nearestArc).directionTo(rc.getLocation()).rotateLeft().rotateLeft());
+						} else {
+							rc.move(arcLoc.get(nearestArc).directionTo(rc.getLocation()).rotateRight().rotateRight());
 						}
 					}
-					else if (left && !right){
-						rc.move(rc.getLocation().directionTo(arcLoc.get(nearestArc)).rotateLeft());
+					if (left && !right) {
+						rc.move(arcLoc.get(nearestArc).directionTo(rc.getLocation()).rotateLeft().rotateLeft());
 					}
-					else if (right && !left){
-						rc.move(rc.getLocation().directionTo(arcLoc.get(nearestArc)).rotateRight());
+					if (right && !left) {
+						rc.move(arcLoc.get(nearestArc).directionTo(rc.getLocation()).rotateRight().rotateRight());
 					}
 				}
 			}
-			else {
-				for (Direction x : Statics.directions)
-					if (rc.canMove(x)) {
-						rc.move(x);
-						break;
-					}
-			}
-	/*		Signal[] signals = rc.emptySignalQueue();
-			for (Signal hej : signals){
-				if (hej.getMessage()[0] == rc.getID()){
-					archon = hej.getLocation();
-				}
-			}
-			if (rc.getLocation().distanceSquaredTo(archon) <4) {
-				if (rc.canMove(archon.directionTo(rc.getLocation()))) {
-					rc.move(archon.directionTo(rc.getLocation())); 
-				}
-			}
-			if (rc.getLocation().distanceSquaredTo(archon) > 9) {
-				if (rc.canMove(rc.getLocation().directionTo(archon))) {
-					rc.move(rc.getLocation().directionTo(archon));
-				}
-			}
-			*/
+			/*
+			 * Signal[] signals = rc.emptySignalQueue(); for (Signal hej :
+			 * signals){ if (hej.getMessage()[0] == rc.getID()){ archon =
+			 * hej.getLocation(); } } if
+			 * (rc.getLocation().distanceSquaredTo(archon) <4) { if
+			 * (rc.canMove(archon.directionTo(rc.getLocation()))) {
+			 * rc.move(archon.directionTo(rc.getLocation())); } } if
+			 * (rc.getLocation().distanceSquaredTo(archon) > 9) { if
+			 * (rc.canMove(rc.getLocation().directionTo(archon))) {
+			 * rc.move(rc.getLocation().directionTo(archon)); } }
+			 */
 		}
 	}
 
@@ -225,54 +194,51 @@ public class SoldierBrain implements Brain {
 			RobotInfo[] enemiesWithinRange = rc.senseNearbyRobots(myAttackRange, enemyTeam);
 			RobotInfo[] zombiesWithinRange = rc.senseNearbyRobots(myAttackRange, Team.ZOMBIE);
 			RobotInfo away = CompareStuff.moveAwayFrom(zombiesWithinRange, rc.getLocation());
-			if(away!=null) {
-				if(rc.isCoreReady()&&rc.canMove(away.location.directionTo(rc.getLocation())))
-					coolMethods.moveTo(CompareStuff.moveAwayFrom(zombiesWithinRange, rc.getLocation()).location.directionTo(rc.getLocation()),rc);
-			}
-			else if (enemiesWithinRange.length > 0) {
+			if (away != null) {
+				if (rc.isCoreReady() && rc.canMove(away.location.directionTo(rc.getLocation())))
+					coolMethods.moveTo(CompareStuff.moveAwayFrom(zombiesWithinRange, rc.getLocation()).location
+							.directionTo(rc.getLocation()), rc);
+			} else if (enemiesWithinRange.length > 0) {
 				// Check if weapon is ready
 				if (rc.isWeaponReady()) {
 					shouldAttack = true;
-					/* RobotInfo lowestHealth = enemiesWithinRange[0];
-					RobotInfo closest = enemiesWithinRange[0];
-					for (RobotInfo r : enemiesWithinRange) {
-						if (lowestHealth.health > r.health)
-							lowestHealth = r;
-						if (rc.getLocation().distanceSquaredTo(closest.location) > rc.getLocation()
-								.distanceSquaredTo(r.location))
-							closest = r;
-					}
-					rc.attackLocation(closest.location); */
+					/*
+					 * RobotInfo lowestHealth = enemiesWithinRange[0]; RobotInfo
+					 * closest = enemiesWithinRange[0]; for (RobotInfo r :
+					 * enemiesWithinRange) { if (lowestHealth.health > r.health)
+					 * lowestHealth = r; if
+					 * (rc.getLocation().distanceSquaredTo(closest.location) >
+					 * rc.getLocation() .distanceSquaredTo(r.location)) closest
+					 * = r; } rc.attackLocation(closest.location);
+					 */
 					rc.attackLocation(CompareStuff.soldierCompare(enemiesWithinRange).location);
-				} 
-			}
-			else if (zombiesWithinRange.length > 0) {				
+				}
+			} else if (zombiesWithinRange.length > 0) {
 				// Check if weapon is ready
 				if (rc.isWeaponReady()) {
 					shouldAttack = true;
-					/* RobotInfo lowestHealth = zombiesWithinRange[0];
-					RobotInfo closest = zombiesWithinRange[0];
-					for (RobotInfo r : zombiesWithinRange) {
-						if (lowestHealth.health > r.health)
-							lowestHealth = r;
-						if (rc.getLocation().distanceSquaredTo(closest.location) > rc.getLocation()
-								.distanceSquaredTo(r.location))
-							closest = r;
-					}
-					rc.attackLocation(closest.location); */			
+					/*
+					 * RobotInfo lowestHealth = zombiesWithinRange[0]; RobotInfo
+					 * closest = zombiesWithinRange[0]; for (RobotInfo r :
+					 * zombiesWithinRange) { if (lowestHealth.health > r.health)
+					 * lowestHealth = r; if
+					 * (rc.getLocation().distanceSquaredTo(closest.location) >
+					 * rc.getLocation() .distanceSquaredTo(r.location)) closest
+					 * = r; } rc.attackLocation(closest.location);
+					 */
 					rc.attackLocation(CompareStuff.soldierCompare(zombiesWithinRange).location);
 				}
 			}
-		if (!shouldAttack) {
-			if (rc.isCoreReady()) {
-				Boolean move = false;
-				Direction lowest = directions[0];
-				double L = rc.senseRubble(rc.getLocation().add(directions[0]));
-				for (int i = (int) (directions.length * Math.random()); i < i + directions.length; i++) {
-					Direction d = directions[i % 8];
-					MapLocation newLoc = rc.getLocation().add(d);
-					if (rc.canMove(d) && rc.senseRubble(newLoc) < GameConstants.RUBBLE_OBSTRUCTION_THRESH) {
-						if (past.contains(newLoc) == false) {
+			if (!shouldAttack) {
+				if (rc.isCoreReady()) {
+					Boolean move = false;
+					Direction lowest = directions[0];
+					double L = rc.senseRubble(rc.getLocation().add(directions[0]));
+					for (int i = (int) (directions.length * Math.random()); i < i + directions.length; i++) {
+						Direction d = directions[i % 8];
+						MapLocation newLoc = rc.getLocation().add(d);
+						if (rc.canMove(d) && rc.senseRubble(newLoc) < GameConstants.RUBBLE_OBSTRUCTION_THRESH) {
+							if (past.contains(newLoc) == false) {
 								move = true;
 								rc.move(d);
 								past.add(newLoc);
@@ -280,19 +246,19 @@ public class SoldierBrain implements Brain {
 									past.remove(0);
 								}
 								break;
-						}
-					} else {
-						if (rc.senseRubble(rc.getLocation().add(d)) < L) {
-							lowest = d;
+							}
+						} else {
+							if (rc.senseRubble(rc.getLocation().add(d)) < L) {
+								lowest = d;
+							}
 						}
 					}
-				}
-				if (!move) {
-					rc.clearRubble(lowest);
-				}
+					if (!move) {
+						rc.clearRubble(lowest);
+					}
 
+				}
 			}
-		}	
 		}
-}
+	}
 }
