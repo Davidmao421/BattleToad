c053881ee348bbfd9fac073c60193000a20c82fa
Alex Wu <alex@Lolz.local>
2016-01-25 21:58:11 -0800

    Revert "Some stuff"
    This reverts commit d16b9d5da37ab2453674f08747b27920a25e033f.

--- battlecode-scaffold-master/src/corners/RobotPlayer.java
+++ battlecode-scaffold-master/src/corners/RobotPlayer.java
@@ -1,15 +1,11 @@
 package corners;
 
-import java.util.LinkedList;
-
 import battlecode.common.Clock;
 import battlecode.common.Direction;
 import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 import battlecode.common.RobotInfo;
-import battlecode.common.RobotType;
-import battlecode.common.Signal;
 
 public class RobotPlayer {
 
@@ -20,7 +16,7 @@ public class RobotPlayer {
 			return true;
 
 		Direction d = enemyCom.directionTo(teamCom);
-		if (!d.isDiagonal()) {
+		if (!d.isDiagonal()){
 			Direction rl = d.rotateLeft();
 			Direction rr = d.rotateRight();
 			if (enemyCom.distanceSquaredTo(teamCom.add(rl)) > enemyCom.distanceSquaredTo(teamCom.add(rr)))
@@ -28,7 +24,7 @@ public class RobotPlayer {
 			else
 				d = rr;
 		}
-
+		
 		MapLocation target = rc.getLocation().add(d);
 		if (rc.canMove(d)) {
 			rc.move(d);
@@ -49,25 +45,27 @@ public class RobotPlayer {
 			d = rl;
 		else
 			d = rr;
-
+		
 		target = rc.getLocation().add(d);
 		System.out.println("\t New direction determined");
-
+		
 		if (rc.canMove(d)) {
 			rc.move(d);
 			return true;
 		}
-
+		
 		for (RobotInfo i : info)
 			if (i.location.equals(target)) {
 				return true;
 			}
-
+		
+		
 		System.out.println("\t Reached corner");
 		return false;
 	}
 
-	public static void archon(RobotController rc) {
+	public static void run(RobotController rc) {
+
 		MapLocation enemyCom = Statics.com(rc.getInitialArchonLocations(rc.getTeam().opponent()));
 		MapLocation teamCom = Statics.com(rc.getInitialArchonLocations(rc.getTeam()));
 
@@ -76,79 +74,6 @@ public class RobotPlayer {
 				Clock.yield();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-	}
-
-	static Signal[] filterBasic(Signal[] signals) {
-		LinkedList<Signal> filtered = new LinkedList<Signal>();
-		for (Signal s : signals)
-			if (s.getMessage() == null)
-				filtered.add(s);
-		return filtered.toArray(new Signal[0]);
-	}
-
-	static MapLocation getHome(Signal[] signals) {
-		for (Signal s : signals)
-			if (s.getMessage() != null && SignalEncoder.getPacketType(s) == PacketType.HOME)
-				return s.getLocation();
-		return null;
-	}
-
-	static boolean guardClear = false;
-
-	public static void guard(RobotController rc) throws GameActionException {
-		if (!rc.isCoreReady())
-			return;
-
-		Signal[] q = rc.emptySignalQueue();
-		guardClear = filterBasic(q).length > 10;
-		if (!guardClear) {
-			MapLocation home = getHome(q);
-			if (home==null){
-				System.out.println("NO HOME");
-				return;
-			}
-			
-			MapLocation target = rc.getLocation().add(home.directionTo(rc.getLocation()));
-			if (home.distanceSquaredTo(target) < RobotType.GUARD.sensorRadiusSquared
-					&& rc.canMove(rc.getLocation().directionTo(target))) {
-				rc.move(rc.getLocation().directionTo(target));
-				return;
-			}
-		}
-	}
-
-	public static void run(RobotController rc) {
-
-		switch (rc.getType()) {
-		case ARCHON:
-			archon(rc);
-			break;
-		case BIGZOMBIE:
-			break;
-		case FASTZOMBIE:
-			break;
-		case GUARD:
-			break;
-		case RANGEDZOMBIE:
-			break;
-		case SCOUT:
-			break;
-		case SOLDIER:
-			break;
-		case STANDARDZOMBIE:
-			break;
-		case TTM:
-			break;
-		case TURRET:
-			break;
-		case VIPER:
-			break;
-		case ZOMBIEDEN:
-			break;
-		default:
-			break;
-
 		}
 
 	}
